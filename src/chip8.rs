f67d5c61b15b350c1a4a40a1a0cd3619d72fd377
ThePhykon <156785466+ThePhykon@users.noreply.github.com>
2025-12-15 13:27:47 +0800

    added test 8XY1

--- src/chip8.rs
+++ src/chip8.rs
@@ -944,4 +944,33 @@ mod opcode_tests {
         // Assert
         assert_eq!(expected, chip);
     }
+
+    #[test]
+    fn test_8XY1() {
+        let cases = [
+            (0x01, 0x00, 0x01),
+            (0x00, 0x01, 0x01),
+            (0x01, 0x01, 0x01),
+            (0x00, 0x00, 0x00),
+        ];
+
+        for (vx, vy, res) in cases {
+            let mut chip = Chip8::new();
+            load_opcode(0x8011, &mut chip);
+
+            // Prepare setup
+            chip.registers[0] = vx;
+            chip.registers[1] = vy;
+
+            let mut expected = chip.clone();
+            expected.pc += 2;
+            expected.registers[0] = res;
+
+            // Run cycle
+            chip.emulateCycle();
+
+            // Assert
+            assert_eq!(expected, chip);
+        }
+    }
 }
